8b9b4c085043eac83f3bb81da70341d22c86ada6
trekin91 <31887259+trekin91@users.noreply.github.com>
2026-02-08 10:51:10 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+// Service Worker pour les notifications
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installé');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activé');
+    event.waitUntil(self.clients.claim());
+});
+
+// Gérer les notifications
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('/')
+    );
+});
